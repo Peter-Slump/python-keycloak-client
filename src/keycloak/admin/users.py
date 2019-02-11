6dd8bb80e3be1e10c42225b89b398652b4e18050
Armin Berres <berres@m2p.net>
2019-02-11 12:07:46 +0100

    Allow to query registered users

--- src/keycloak/admin/users.py
+++ src/keycloak/admin/users.py
@@ -53,3 +53,45 @@ class Users(KeycloakAdminBase):
             ),
             data=json.dumps(payload)
         )
+
+    def all(self):
+        """
+        Return all registered users
+
+        http://www.keycloak.org/docs-api/3.4/rest-api/index.html#_users_resource
+        """
+        return self._client.get(
+            url=self._client.get_full_url(
+                self.get_path('collection', realm=self._realm_name)
+            )
+        )
+
+    def by_id(self, user_id):
+        return User(realm_name=self._realm_name,
+                    user_id=user_id, client=self._client)
+
+
+class User(KeycloakAdminBase):
+    _paths = {
+        'single': '/auth/admin/realms/{realm}/users/{user_id}'
+    }
+
+    def __init__(self, realm_name, user_id, *args, **kwargs):
+        self._realm_name = realm_name
+        self._user_id = user_id
+
+        super(User, self).__init__(*args, **kwargs)
+
+    def get(self):
+        """
+        Return registered user with the given user id.
+
+        http://www.keycloak.org/docs-api/3.4/rest-api/index.html#_users_resource
+        """
+        return self._client.get(
+            url=self._client.get_full_url(
+                self.get_path(
+                    'single', realm=self._realm_name, user_id=self._user_id
+                )
+            )
+        )
