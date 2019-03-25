e7c8f14402c44638103f3df6ad3cad41d66e38fe
JRosa <j.rosa@telic.de>
2019-03-25 12:17:31 +0100

    add groups for user, rename clientRole, add userRole, add delete in KeycloakAdmin class

---
+++ src/keycloak/admin/userroles.py
@@ -0,0 +1,36 @@
+import json
+
+from keycloak.admin import KeycloakAdminBase
+
+__all__ = ('UserRoles',)
+
+
+class UserRoles(KeycloakAdminBase):
+    _paths = {
+        'availableRealm': '/auth/admin/realms/{realm}/users/{id}/role-mappings/realm/available',
+        'roleRealm': '/auth/admin/realms/{realm}/users/{id}/role-mappings/realm'
+    }
+
+    def __init__(self, realm_name, user_id, *args, **kwargs):
+        self._realm_name = realm_name
+        self._user_id = user_id
+        super(UserRoles, self).__init__(*args, **kwargs)
+
+    def available_realm_role(self):
+        return self._client.get(
+            url=self._client.get_full_url(
+                self.get_path(
+                    'availableRealm', realm=self._realm_name, id=self._user_id
+                )
+            )
+        )
+
+    def add_realm_role(self, roles):
+        return self._client.post(
+            url=self._client.get_full_url(
+                self.get_path(
+                    'roleRealm', realm=self._realm_name, id=self._user_id
+                )
+            ),
+            data=json.dumps([roles])
+        )
--- src/keycloak/admin/users.py
+++ src/keycloak/admin/users.py
@@ -5,6 +5,17 @@ from keycloak.admin import KeycloakAdminBase
 
 __all__ = ('Users',)
 
+USER_KWARGS = [
+    'email',
+    'firstName',
+    'lastName',
+    'emailVerified',
+    'attributes',
+    'realmRoles',
+    'clientRoles',
+    'groups'
+]
+
 
 class Users(KeycloakAdminBase):
     _paths = {
@@ -73,14 +84,26 @@ class Users(KeycloakAdminBase):
 
 class User(KeycloakAdminBase):
     _paths = {
-        'single': '/auth/admin/realms/{realm}/users/{user_id}'
+        'single': '/auth/admin/realms/{realm}/users/{user_id}',
+        'group': '/auth/admin/realms/{realm}/users/{user_id}/groups',
+        'groupAdd': '/auth/admin/realms/{realm}/users/{user_id}/groups/{group_id}'
     }
 
     def __init__(self, realm_name, user_id, *args, **kwargs):
         self._realm_name = realm_name
         self._user_id = user_id
-
+        self._it = None
         super(User, self).__init__(*args, **kwargs)
+
+    @property
+    def it(self):
+        if self._it is None:
+            self.get()
+        return self._it
+
+    def roles(self):
+        from keycloak.admin.clientroles import ClientRoles
+        return ClientRoles(realm_name=self._realm_name, user_id=self._user_id, client=self._client)
 
     def get(self):
         """
@@ -88,10 +111,86 @@ class User(KeycloakAdminBase):
 
         http://www.keycloak.org/docs-api/3.4/rest-api/index.html#_users_resource
         """
-        return self._client.get(
+        self._it = self._client.get(
             url=self._client.get_full_url(
                 self.get_path(
                     'single', realm=self._realm_name, user_id=self._user_id
+                )
+            )
+        )
+        self._user_id = self.it["id"]
+        return self._it
+
+    def update(self, **kwargs):
+        """
+                Update existing user.
+
+                https://www.keycloak.org/docs-api/2.5/rest-api/index.html#_userrepresentation
+
+                :param str firstName: Firstname for user
+                :param str lastName: LastName for user
+                :param str email: Email for user
+                :param bool emailVerified: User email verified
+                :param Map attributes: Atributes in user
+                :param string array realmRoles: Realm Roles
+                :param Map clientRoles: Client Roles
+                :param string array groups: Groups for user
+                """
+        payload = OrderedDict()
+        for k, v in self.it.items():
+            payload[k] = v
+        for key in USER_KWARGS:
+            from keycloak.admin.clientroles import to_camel_case
+            if key in kwargs:
+                payload[to_camel_case(key)] = kwargs[key]
+        print("*DEBUG* Update User:" + str(json.dumps(payload)))
+        result = self._client.put(
+            url=self._client.get_full_url(
+                self.get_path(
+                    'single', realm=self._realm_name, user_id=self._user_id
+                )
+            ),
+            data=json.dumps(payload)
+        )
+        self.get()
+        return result
+
+    def user_groups(self):
+        return self._client.get(
+            url=self._client.get_full_url(
+                self.get_path(
+                    'group',
+                    realm=self._realm_name,
+                    user_id=self._user_id
+                )
+            )
+        )
+
+    def add_to_group(self, group_id):
+        return self._client.put(
+            url=self._client.get_full_url(
+                self.get_path(
+                    'groupAdd',
+                    realm=self._realm_name,
+                    user_id=self._user_id,
+                    group_id=group_id
+                )
+            ),
+            data=json.dumps({
+                "realm": self._realm_name,
+                "userId": self._user_id,
+                "groupId": group_id
+            })
+        )
+
+    def delete_group(self, group_id):
+        return self._client.delete(
+            url=self._client.get_full_url(
+                self.get_path(
+                    'groupAdd',
+                    realm=self._realm_name,
+                    user_id=self._user_id,
+                    group_id=group_id
                 )
             )
         )
