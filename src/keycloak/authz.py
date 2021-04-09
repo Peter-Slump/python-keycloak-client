22d87577f4637133146c376dbb99553035b340ab
Q <q@misell.cymru>
2021-04-09 17:50:28 +0100

    fix authz token

--- src/keycloak/authz.py
+++ src/keycloak/authz.py
@@ -115,7 +115,7 @@ class KeycloakAuthz(WellKnownMixin, object):
                     response.get('error_description')
                 )
             else:
-                token = response.get('refresh_token')
+                token = response.get('access_token')
                 decoded_token = self._decode_token(token.split('.')[1])
                 authz_info = decoded_token.get('authorization', {})
         except KeycloakClientError as error:
